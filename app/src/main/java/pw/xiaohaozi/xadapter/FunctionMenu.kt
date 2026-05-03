c84d9004ac174d5a324c64efc26260bead996c53
xiaohaozi9825 <982528257@qq.com>
2026-05-04 02:01:36 +0800

    refactor:演示列表增加封面图

--- app/src/main/java/pw/xiaohaozi/xadapter/FunctionMenu.kt
+++ app/src/main/java/pw/xiaohaozi/xadapter/FunctionMenu.kt
@@ -39,28 +39,28 @@ import pw.xiaohaozi.xadapter.info.HomeInfo
 //smart模块
 val smartMenuList = arrayListOf(
     "Adapter创建",
-    HomeInfo("创建单布局", "单布局创建方式", R.mipmap.ic_launcher, SingleFragment::class.java),
-    HomeInfo("创建多布局", "多布局创建方式", R.mipmap.ic_launcher, MultipleFragment::class.java),
+    HomeInfo("创建单布局", "单布局创建方式", R.mipmap.icon_smart_single, SingleFragment::class.java),
+    HomeInfo("创建多布局", "多布局创建方式", R.mipmap.icon_smart_multiple, MultipleFragment::class.java),
     "选择操作",
-    HomeInfo("选择操作", "Item选择操作", R.mipmap.ic_launcher, SelectFragment::class.java),
+    HomeInfo("选择操作", "Item选择操作", R.mipmap.icon_smart_select, SelectFragment::class.java),
     "Item事件监听",
-    HomeInfo("点击事件", "item点击事件", R.mipmap.ic_launcher, ClickFragment::class.java),
-    HomeInfo("长按事件", "item长按事件", R.mipmap.ic_launcher, LongClickFragment::class.java),
-    HomeInfo("选中事件", "单选、多选等", R.mipmap.ic_launcher, CheckFragment::class.java),
-    HomeInfo("文本变化", "EditText文本变化监听", R.mipmap.ic_launcher, TextChangeFragment::class.java),
+    HomeInfo("点击事件", "item点击事件", R.mipmap.icon_smart_click, ClickFragment::class.java),
+    HomeInfo("长按事件", "item长按事件", R.mipmap.icon_smart_long_click, LongClickFragment::class.java),
+    HomeInfo("选中事件", "单选、多选等", R.mipmap.icon_smart_check, CheckFragment::class.java),
+    HomeInfo("文本变化", "EditText文本变化监听", R.mipmap.icon_smart_text_change, TextChangeFragment::class.java),
     "特殊布局",
-    HomeInfo("特殊布局", "如头布局，脚布局，空布局、缺省页", R.mipmap.ic_launcher, SpecialLayoutFragment::class.java),
-    HomeInfo("分组布局", "允许Item撑满整行", R.mipmap.ic_launcher, GroupFragment::class.java),
+    HomeInfo("特殊布局", "如头布局，脚布局，空布局、缺省页", R.mipmap.icon_smart_special, SpecialLayoutFragment::class.java),
+    HomeInfo("分组布局", "允许Item撑满整行", R.mipmap.icon_smart_group, GroupFragment::class.java),
     "拖拽与侧滑",
-    HomeInfo("侧滑删除", "侧滑删除Item", R.mipmap.ic_launcher, SwipeDeleteFragment::class.java),
-    HomeInfo("拖拽排序", "长按拖拽排序", R.mipmap.ic_launcher, DragSortFragment::class.java),
-    HomeInfo("侧滑菜单", "类似QQ侧滑效果", R.mipmap.ic_launcher, SwipeMenuFragment::class.java),
+    HomeInfo("侧滑删除", "侧滑删除Item", R.mipmap.icon_smart_swipe_delete, SwipeDeleteFragment::class.java),
+    HomeInfo("拖拽排序", "长按拖拽排序", R.mipmap.icon_smart_draw_sort, DragSortFragment::class.java),
+    HomeInfo("侧滑菜单", "类似QQ侧滑效果", R.mipmap.icon_smart_swipe_menu, SwipeMenuFragment::class.java),
     "数据操作",
-    HomeInfo("常规操作", "数据增删改查", R.mipmap.ic_launcher, DataOperationFragment::class.java),
-    HomeInfo("Differ", "使用Differ更新数据", R.mipmap.ic_launcher, DataDifferFragment::class.java),
+    HomeInfo("常规操作", "数据增删改查", R.mipmap.icon_smart_data_operation, DataOperationFragment::class.java),
+    HomeInfo("Differ", "使用Differ更新数据", R.mipmap.icon_smart_data_differ, DataDifferFragment::class.java),
     "其他",
-    HomeInfo("协程测试", "", R.mipmap.ic_launcher, CoroutineScopeFragment::class.java),
-    HomeInfo("ConcatAdapter", "结合ConcatAdapter使用", R.mipmap.ic_launcher, ConcatAdapterFragment::class.java),
+    HomeInfo("协程作用域", "", R.mipmap.icon_smart_coroutine, CoroutineScopeFragment::class.java),
+    HomeInfo("ConcatAdapter", "结合ConcatAdapter使用", R.mipmap.icon_smart_concat, ConcatAdapterFragment::class.java),
 )
 
 //node模块
@@ -70,67 +70,68 @@ val nodeMenuList = arrayListOf(
     HomeInfo("node编辑", "", R.mipmap.ic_launcher, NodeEditFragment::class.java),
     HomeInfo("node编辑2", "", R.mipmap.ic_launcher, Node2EditFragment::class.java),
     "Node创建",
-    HomeInfo("单类型创建", "单类型Node创建", R.mipmap.ic_launcher, CreateNodeFragment::class.java),
-    HomeInfo("多类型创建", "多类型Node创建", R.mipmap.ic_launcher, CreateMultipleNodeFragment::class.java),
+    HomeInfo("单类型创建", "单类型Node创建", R.mipmap.icon_smart_single, CreateNodeFragment::class.java),
+    HomeInfo("多类型创建", "多类型Node创建", R.mipmap.icon_node_multiple, CreateMultipleNodeFragment::class.java),
     "Node展开与折叠",
-    HomeInfo("展开与折叠", "Node展开与折叠", R.mipmap.ic_launcher),
+    HomeInfo("展开与折叠", "Node展开与折叠", R.mipmap.icon_smart_multiple),
 
     "Node数据操作",
-    HomeInfo("添加一个节点", "", R.mipmap.ic_launcher, AddNodeFragment::class.java),
-    HomeInfo("添加多个节点", "", R.mipmap.ic_launcher, AddNodesFragment::class.java),
+    HomeInfo("添加一个节点", "", R.mipmap.icon_node_add, AddNodeFragment::class.java),
+    HomeInfo("添加多个节点", "", R.mipmap.icon_node_add, AddNodesFragment::class.java),
 
     HomeInfo(
         "修改节点",
         "修改指定节点内容",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_edit,
         EditNodeFragment::class.java
     ),//updateNode(node: D, payload: Any? = null)、updateNode(oldNode: D, newNode: D, payload: Any? = null)、updateChildNode(parent: D, oldNode: D, newNode: D, payload: Any? = null)
     HomeInfo(
         "替换Node",
         "替换节点以及子节点",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_replace,
         ReplaceNodeFragment::class.java
     ),//replaceNode(oldNode: D, newNode: D)
 
     HomeInfo(
         "删除一个节点",
         "删除一个根节点或子节点",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_delete,
         RemoveNodeFragment::class.java
     ),//removeNode(node: D) 、removeChildNode(parent: D, node: D)
     HomeInfo(
         "按索引删除节点",
         "",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_delete,
         RemoveAtNodeFragment::class.java
     ),//removeNodeAt(index: Int)、removeChildNodeAt(parent: D, index: Int)
     HomeInfo(
         "删除多个连续节点",
         "",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_delete,
         RemoveMultipleNodeFragment::class.java
     ),//removeNode(start: Int, count: Int)、removeChildNode(parent: D, start: Int, count: Int)
     HomeInfo(
         "删除多个不连续节点",
         "",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_delete,
         RemoveListNodeFragment::class.java
     ),//removeNodeList(nodes: List<D>)、 removeChildNodeList(parent: D, nodes: List<D>)
     HomeInfo(
         "按列表所在位置删除",
         "",
-        R.mipmap.ic_launcher,
+        R.mipmap.icon_node_delete,
         RemovePositionNodeFragment::class.java
     ),//removeNodePosition(adapterPosition: Int)、
 )
 
 //示例模块
 val exampleMenuList = arrayListOf<Any>(
-    HomeInfo("好友列表", "仿QQ好友列表", R.mipmap.ic_launcher, FriendFragment::class.java),
-    HomeInfo("分类列表联动", "商品分类用与商品列表联动", R.mipmap.ic_launcher, GoodsFragment::class.java),
-    HomeInfo("多级分类列表", "商品分类多级菜单", R.mipmap.ic_launcher,MultipleCategoryFragment::class.java),
-    HomeInfo("图片选择器", "", R.mipmap.ic_launcher,ImageSelectedFragment::class.java),
-    HomeInfo("底部导航栏", "", R.mipmap.ic_launcher),
-    HomeInfo("TabBar", "", R.mipmap.ic_launcher),
+    HomeInfo("好友列表", "仿QQ好友列表", R.mipmap.icon_example_friend, FriendFragment::class.java),
+    HomeInfo("分类列表联动", "商品分类用与商品列表联动", R.mipmap.icon_example_goods_category, GoodsFragment::class.java),
+    HomeInfo("多级分类列表", "商品分类多级菜单", R.mipmap.icon_example_goods_category,MultipleCategoryFragment::class.java),
+    HomeInfo("图片选择器", "防微信图片选择器", R.mipmap.icon_example_image_selected,ImageSelectedFragment::class.java),
+    HomeInfo("标签", "", R.mipmap.icon_example_tag),
+//    HomeInfo("底部导航栏", "", R.mipmap.ic_launcher),
+//    HomeInfo("TabBar", "", R.mipmap.ic_launcher),
 
     )
